6ceb94d9add424f146fee54c9558656b5bec47bb
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2025-07-27 16:44:48 +0100

    make java interaction thingy

--- android/app/src/main/java/com/zenyfh/zenmusic/MainActivity.java
+++ android/app/src/main/java/com/zenyfh/zenmusic/MainActivity.java
@@ -1,6 +1,25 @@
 package com.zenyfh.zenmusic;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "com.zenyfh.zenmusic/hello";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+            (call, result) -> {
+                if (call.method.equals("getMessage")) {
+                    result.success("Java hello. 1");
+                    return;
+                }
+                result.notImplemented();
+            }
+        );
+    }
 }

e1fb06c65403de54e87b1f12d8a03e3ac02e9c1b
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2025-07-28 23:30:40 +0100

    getters and setters

--- android/app/src/main/java/com/zenyfh/zenmusic/audio/AudioTrack.java
+++ android/app/src/main/java/com/zenyfh/zenmusic/audio/AudioTrack.java
@@ -1,19 +1,56 @@
 package com.zenyfh.zenmusic.audio;
 
 import android.net.Uri;
+import org.jetbrains.annotations.NotNull;
 
 public class AudioTrack {
-    public String artist;
-    public String name;
-    public Uri thumbnail;
-    public int length; // in seconds
-    public String streamUrl; // required for playback
+    private final String artist;
+    private final String title;
+    private final Uri thumbnail;
+    private final int length;   // seconds
+    private int position;       // seconds
+    private final String streamUrl;
 
-    public AudioTrack(String artist, String name, Uri thumbnail, int length, String streamUrl) {
+    public AudioTrack(String artist, String title, Uri thumbnail, int length, int position, String streamUrl) {
         this.artist = artist;
-        this.name = name;
+        this.title = title;
         this.thumbnail = thumbnail;
         this.length = length;
+        this.position = position;
         this.streamUrl = streamUrl;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Uri getThumbnail() {
+        return thumbnail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getStreamUrl() {
+        return streamUrl;
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return String.format("AudioTrack(%s - %s)", artist, title);
     }
 }
